61e80f2913f40a4fd9124cd0a56cb8b539e55d90
Patrick Provost <wawaron@runbox.com>
2022-04-08 01:11:36 -0400

    The Draw One Square Project

--- src/core.js
+++ src/core.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import * as vertexBuffer from "./vertex_buffer.js";
+import * as simpleShader from "./shader_support.js";
+
 let mGL = null;
 function getGL() { return mGL; }
 
@@ -11,13 +14,24 @@ function initWebGL(htmlCanvasID) {
         return;
     }
     mGL.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    vertexBuffer.init();
+    simpleShader.init("VertexShader", "FragmentShader");
 }
 
 function clearCanvas() {
     mGL.clear(mGL.COLOR_BUFFER_BIT);
 }
 
+function drawSquare() {
+    simpleShader.activate();
+    mGL.drawArrays(mGL.TRIANGLE_STRIP, 0, 4);
+}
+
 window.onload = function() {
     initWebGL("GLCanvas");
     clearCanvas();
+    drawSquare();
 };
+
+export { getGL };
